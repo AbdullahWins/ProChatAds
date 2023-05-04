f37849deb62dd6517e2569b957e9ce770a7eed61
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-04 14:10:53 +0600

    added divider

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,7 +1,7 @@
 import React from "react";
 import qrCode from "../../assets/qr/QR.png";
 import appStore from "../../assets/img/appStore.svg";
-import android from "../../assets/img/playStore.svg";
+import playStore from "../../assets/img/playStore.svg";
 import apk from "../../assets/img/apk.svg";
 import line from "../../assets/img/line.svg";
 
@@ -9,7 +9,7 @@ const Download = () => {
   return (
     <section className="w-full max-w-6xl p-4">
       <hr className="opacity-20 text-btnColor" />
-      <div className="grid md: grid-cols-1 md:grid-cols-6 gap-8 items-center justify-between py-4">
+      <div className="grid md: grid-cols-1 md:grid-cols-6 items-center justify-between py-4">
         <div className="col-span-1 md:col-span-4 flex flex-col items-center justify-center gap-1">
           <p className="text-2xl font-bold">Download Now</p>
           <p className="text-base text-center">
@@ -17,13 +17,34 @@ const Download = () => {
             AppStore or Google Play.
           </p>
           <div className="flex flex-col md:flex-row items-center justify-center gap-4 py-6">
-            <img src={appStore} alt="" />
-            <img src={android} alt="" />
-            <img src={apk} alt="" />
+            <a
+              href="https://stackoverflow.com/users/19174477/abdullahwins"
+              target="blank"
+            >
+              <button className="">
+                <img src={appStore} alt="" />
+              </button>
+            </a>
+            <a
+              href="https://stackoverflow.com/users/19174477/abdullahwins"
+              target="blank"
+            >
+              <button className="">
+                <img src={playStore} alt="" />
+              </button>
+            </a>
+            <a
+              href="https://stackoverflow.com/users/19174477/abdullahwins"
+              target="blank"
+            >
+              <button className="">
+                <img src={apk} alt="" />
+              </button>
+            </a>
           </div>
         </div>
-        <div className="col-span-1">
-          <img src={line} alt="" />
+        <div className="hidden col-span-1 md:flex justify-center opacity-30 pt-12">
+          <img className="h-28" src={line} alt="" />
         </div>
         <div className="col-span-1 flex flex-col items-center justify-center">
           <p className="text-lg font-bold pb-2">Scan QR code</p>
